2406003e5cf0ff1072d3712a8fdb73a67f4b2aa9
marosimosi <marosimosi@gmail.com>
2023-05-01 23:36:42 +0300

    preparing data

--- question2.py
+++ question2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # Load the CSV file
 df = pd.read_csv("data.csv")
@@ -15,9 +16,6 @@ df['Cases'] = df['Cases'].fillna(0)
 # Fill missing values in "Deaths" column with 0
 df['Deaths'] = df['Deaths'].fillna(0)
 
-inf = (df['Daily tests'] == 0).sum()
-print("\n \nInfinite daily tests:", inf)
-
 # convert date column to datetime format
 df['Date'] = pd.to_datetime(df['Date'])
 
@@ -29,14 +27,35 @@ grouped = df.groupby(['Entity', pd.Grouper(key='Date', freq='W-MON')]).agg({
     'Population': 'max'
 })
 
+# get the first week of each country
+first_week = grouped.groupby('Entity').head(1)
+
 # calculate the new columns
 grouped['Cases/tests per week'] = grouped['Cases'].diff() / grouped['Daily tests']
 grouped['Deaths/cases per week'] = grouped['Deaths'].diff() / grouped['Cases'].diff()
 grouped['Tests/population per week'] = grouped['Daily tests'] / grouped['Population']
 
-# reset the index to make the country and week columns regular columns
+# remove the first week from the data(we don't have the previous week to calculate the difference)
+grouped = grouped.drop(first_week.index)
+# calculate the new columns for the first week
+first_week['Cases/tests per week'] = first_week['Cases'] / first_week['Daily tests']
+first_week['Deaths/cases per week'] = first_week['Deaths'] / first_week['Cases']
+first_week['Tests/population per week'] = first_week['Daily tests'] / first_week['Population']
+
+# add the first week to the start of the data
+grouped = pd.concat([first_week, grouped])
+
+# reset the index
 grouped = grouped.reset_index()
 
-# select the desired columns and save to a new csv file
 new_df = grouped[['Entity', 'Cases/tests per week', 'Deaths/cases per week', 'Tests/population per week']]
+# remove inf values (no cases in a week)
+new_df = new_df[new_df['Deaths/cases per week'] != np.inf]
+# remove nan values (no cases and no deaths in a week)
+new_df = new_df.dropna()
+# remove negative values
+new_df.drop(new_df[new_df['Cases/tests per week'] < 0].index, inplace = True)
+new_df.drop(new_df[new_df['Deaths/cases per week'] < 0].index, inplace = True)
+stats = new_df.describe()
+print(stats)
 new_df.to_csv('new_dataset.csv', index=False)
