eefbca4498daada20b6442161c23dd96634cc23b
Alex Tsaparas <up1072824@upnet.gr>
2023-05-01 23:57:43 +0300

    1st try with KMeans

--- question2.py
+++ question2.py
@@ -1,5 +1,11 @@
 import pandas as pd
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import zscore
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
 
 # Load the CSV file
 df = pd.read_csv("data.csv")
@@ -59,3 +65,34 @@ new_df.drop(new_df[new_df['Deaths/cases per week'] < 0].index, inplace = True)
 stats = new_df.describe()
 print(stats)
 new_df.to_csv('new_dataset.csv', index=False)
+
+# Scale the features
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(new_df)
+
+# Elbow method to determine the number of clusters
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
+    kmeans.fit(X_scaled)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+# Fit the KMeans algorithm to the data
+kmeans = KMeans(n_clusters=3, init='k-means++', random_state=42)
+y_kmeans = kmeans.fit_predict(X_scaled)
+
+# Visualize the clusters
+plt.scatter(X_scaled[y_kmeans == 0, 0], X_scaled[y_kmeans == 0, 1], s=100, c='red', label='Cluster 1')
+plt.scatter(X_scaled[y_kmeans == 1, 0], X_scaled[y_kmeans == 1, 1], s=100, c='blue', label='Cluster 2')
+plt.scatter(X_scaled[y_kmeans == 2, 0], X_scaled[y_kmeans == 2, 1], s=100, c='green', label='Cluster 3')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label='Centroids')
+plt.title('Clusters of Countries')
+plt.xlabel('Positivity Rate')
+plt.ylabel('Death Rate')
+plt.legend()
+plt.show()
