0532bf8b8e994c7de03eb399a563cbb6dbf19198
Alex Tsaparas <up1072824@upnet.gr>
2023-05-08 15:09:59 +0300

    Elbow Method with yellowbrick library
    Run pip install yellowbrick

--- question2.py
+++ question2.py
@@ -6,6 +6,7 @@ import seaborn as sns
 from scipy.stats import zscore
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
+from yellowbrick.cluster import KElbowVisualizer
 
 # Load the CSV file
 df = pd.read_csv("data.csv")
@@ -15,8 +16,8 @@ df.drop(df[df['Daily tests'] < 0].index, inplace = True)
 
 # --- Fill missing values in columns
 # Fill missing values in "Daily tests" column
-df['Daily tests'] = df['Daily tests'].groupby(df['Entity']).apply(lambda x: x.fillna(method='ffill'))
-df['Daily tests'] = df['Daily tests'].groupby(df['Entity']).apply(lambda x: x.fillna(method='bfill'))
+df['Daily tests'] = df['Daily tests'].groupby(df['Entity']).apply(lambda x: x.fillna(method='ffill')).reset_index(drop=True)
+df['Daily tests'] = df['Daily tests'].groupby(df['Entity']).apply(lambda x: x.fillna(method='bfill')).reset_index(drop=True)
 # Fill missing values in "Cases" column with 0
 df['Cases'] = df['Cases'].fillna(0)
 # Fill missing values in "Deaths" column with 0
@@ -37,7 +38,7 @@ grouped = df.groupby(['Entity', pd.Grouper(key='Date', freq='W-MON')]).agg({
 first_week = grouped.groupby('Entity').head(1)
 
 # calculate the new columns
-grouped['Cases/tests per week'] = grouped['Cases'].diff() / grouped['Daily tests']
+grouped['Cases/tests per week'] = np.where(grouped['Daily tests'] != 0, grouped['Cases'].diff() / grouped['Daily tests'], 0)
 grouped['Deaths/cases per week'] = grouped['Deaths'].diff() / grouped['Cases'].diff()
 grouped['Tests/population per week'] = grouped['Daily tests'] / grouped['Population']
 
@@ -64,35 +65,22 @@ new_df.drop(new_df[new_df['Cases/tests per week'] < 0].index, inplace = True)
 new_df.drop(new_df[new_df['Deaths/cases per week'] < 0].index, inplace = True)
 stats = new_df.describe()
 print(stats)
+
+# Use one hot encoding to convert categorical data to numerical data
+new_df = pd.get_dummies(new_df, columns=['Entity'])
+
 new_df.to_csv('new_dataset.csv', index=False)
 
-# Scale the features
+
+# --- CLUSTERING ---
+# Standardize the data
 scaler = StandardScaler()
-X_scaled = scaler.fit_transform(new_df)
+X = scaler.fit_transform(new_df.values)
 
 # Elbow method to determine the number of clusters
-wcss = []
-for i in range(1, 11):
-    kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
-    kmeans.fit(X_scaled)
-    wcss.append(kmeans.inertia_)
-plt.plot(range(1, 11), wcss)
-plt.title('Elbow Method')
-plt.xlabel('Number of clusters')
-plt.ylabel('WCSS')
-plt.show()
-
-# Fit the KMeans algorithm to the data
-kmeans = KMeans(n_clusters=3, init='k-means++', random_state=42)
-y_kmeans = kmeans.fit_predict(X_scaled)
-
-# Visualize the clusters
-plt.scatter(X_scaled[y_kmeans == 0, 0], X_scaled[y_kmeans == 0, 1], s=100, c='red', label='Cluster 1')
-plt.scatter(X_scaled[y_kmeans == 1, 0], X_scaled[y_kmeans == 1, 1], s=100, c='blue', label='Cluster 2')
-plt.scatter(X_scaled[y_kmeans == 2, 0], X_scaled[y_kmeans == 2, 1], s=100, c='green', label='Cluster 3')
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label='Centroids')
-plt.title('Clusters of Countries')
-plt.xlabel('Positivity Rate')
-plt.ylabel('Death Rate')
-plt.legend()
-plt.show()
+# Instantiate the clustering model and visualizer
+kmeans = KMeans(init='k-means++', n_init=10)
+visualizer = KElbowVisualizer(kmeans, k=(2,10))
+ 
+visualizer.fit(X)
+visualizer.show()
